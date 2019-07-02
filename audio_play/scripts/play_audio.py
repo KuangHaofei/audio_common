3551463b0f11dbd1979ca6cff16c2b8492d5d77a
Operator Computer <operator-computer@example.com>
2019-07-02 18:56:37 +0800

    update

--- audio_play/scripts/play_audio.py
+++ audio_play/scripts/play_audio.py
@@ -5,7 +5,7 @@ import os
 if __name__ == '__main__':
     rospy.init_node("audio_listener", anonymous=True)
 
-    port = rospy.get_param('port', "4001")
+    port = rospy.get_param('port', "5001")
 
     os.system("gst-launch-1.0 udpsrc port={} caps=\"application/x-rtp\" ! rtppcmadepay ! alawdec ! alsasink"
               .format(port))
--- audio_play/scripts/record_audio.py
+++ audio_play/scripts/record_audio.py
@@ -4,8 +4,8 @@ import os
 
 if __name__ == '__main__':
     rospy.init_node("audio_talker", anonymous=True)
-    host = rospy.get_param("host", "192.168.43.217")
-    port = rospy.get_param("port", "5001")
+    host = rospy.get_param("host", "192.168.1.11")
+    port = rospy.get_param("port", "4001")
     os.system("gst-launch-1.0 autoaudiosrc ! audioconvert ! audioresample ! alawenc ! rtppcmapay ! udpsink "
               "host={} port={}".format(host, port))
 
